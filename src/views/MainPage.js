c7ac3eec2e7ab6a7c4ff09c1eea1c1c178c6aed3
mohamedhesham221 <mohmadetsh874@gmail.com>
2023-01-22 19:05:10 +0200

    first commit

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -2,31 +2,34 @@ import { Link } from "react-router-dom";
 import CurrRead from "../components/book-shelf/CurrRead";
 import WantToRead from "../components/book-shelf/WantToRead";
 import Read from "../components/book-shelf/Read";
-import { useState } from "react";
-import * as BooksAPI from "../BooksApi"
+import { useEffect, useState } from "react";
+import * as BooksAPI from "../BooksApi";
 
 const BookShelfPage = () => {
   const [currentlyReading, setCrrBooks] = useState([]);
-  const [wantToRead, setWantToBooks]= useState([]);
-  const [read , setRead]= useState([]);
+  const [wantToRead, setWantToBooks] = useState([]);
+  const [read, setRead] = useState([]);
 
-  // get all the books from the backend server and and filter it into 3 shelves 
+  // get all the books from the backend server and and filter it into 3 shelves
+  useEffect(() => {
     BooksAPI.getAll().then((books) => {
-      setCrrBooks(books.filter(book=> book.shelf === "currentlyReading"))
-      setWantToBooks(books.filter(book=> book.shelf === "wantToRead"))
-      setRead(books.filter(book=> book.shelf === "read"))
-    })
-
+      setCrrBooks(books.filter((book) => book.shelf === "currentlyReading"));
+      setWantToBooks(books.filter((book) => book.shelf === "wantToRead"));
+      setRead(books.filter((book) => book.shelf === "read"));
+    });
+  }, []);
   return (
     <>
       <header className="reads-header">
         <h1>MyReads</h1>
       </header>
       <div className="container">
-        <Link to="/search-page" className="add-book" title="add new book">✚</Link>
+        <Link to="/search-page" className="add-book" title="add new book">
+          ✚
+        </Link>
         <CurrRead shelf={currentlyReading} />
         <WantToRead shelf={wantToRead} />
-        <Read shelf ={read} />
+        <Read shelf={read} />
       </div>
     </>
   );
